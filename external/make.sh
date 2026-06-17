5d2821b1f248d7b1dc0cd866384a2e0a66a4be05
vasilbp20-lgtm <vasilbp20@gmail.com>
2026-06-17 22:33:19 +0300

    Update make.sh

--- external/make.sh
+++ external/make.sh
@@ -4,7 +4,6 @@
 
 # shellcheck disable=SC1007,SC2164,SC2181,SC2291
 
-# [
 BUILD()
 {
     local PDR
@@ -63,18 +62,13 @@ GET_CMAKE_FLAGS()
     echo "$FLAGS"
 }
 
-# https://android.googlesource.com/platform/build/+/refs/tags/android-15.0.0_r1/envsetup.sh#18
 GET_SRC_DIR()
 {
     local TOPFILE="unica/configs/version.sh"
     if [ -n "$SRC_DIR" ] && [ -f "$SRC_DIR/$TOPFILE" ]; then
-        # The following circumlocution ensures we remove symlinks from SRC_DIR.
         (cd "$SRC_DIR"; PWD= /bin/pwd)
     else
         if [ -f "$TOPFILE" ]; then
-            # The following circumlocution (repeated below as well) ensures
-            # that we record the true directory name and not one that is
-            # faked up with symlink names.
             PWD= /bin/pwd
         else
             local HERE="$PWD"
@@ -91,7 +85,6 @@ GET_SRC_DIR()
     fi
 }
 
-# https://github.com/canonical/snapd/blob/ec7ea857712028b7e3be7a5f4448df575216dbfd/release/release.go#L169-L190
 IS_WSL()
 {
     if [ -e "/proc/sys/fs/binfmt_misc/WSLInterop" ] || [ -e "/run/WSL" ]; then
@@ -100,7 +93,6 @@ IS_WSL()
         echo "OFF"
     fi
 }
-# ]
 
 SRC_DIR="$(GET_SRC_DIR)"
 if [ ! "$SRC_DIR" ]; then
@@ -121,123 +113,33 @@ IMG2SDAT=true
 SAMLOADER=true
 SIGNAPK=true
 
-ANDROID_TOOLS_EXEC=(
-    "adb" "append2simg" "avbtool" "e2fsdroid"
-    "ext2simg" "fastboot" "fec" "gki/generate_gki_certificate.py"
-    "img2simg" "lpadd" "lpdump" "lpflash" "lpmake"
-    "lpunpack" "make_f2fs" "mkbootfs" "mkbootimg" "mkdtboimg" "mke2fs"
-    "mke2fs.android" "mke2fs.conf" "mkf2fsuserimg" "mkuserimg_mke2fs"
-    "repack_bootimg" "simg2img" "sload_f2fs" "unpack_bootimg" "zipalign"
-)
+ANDROID_TOOLS_EXEC=("adb" "append2simg" "avbtool" "e2fsdroid" "ext2simg" "fastboot" "fec" "gki/generate_gki_certificate.py" "img2simg" "lpadd" "lpdump" "lpflash" "lpmake" "lpunpack" "make_f2fs" "mkbootfs" "mkbootimg" "mkdtboimg" "mke2fs" "mke2fs.android" "mke2fs.conf" "mkf2fsuserimg" "mkuserimg_mke2fs" "repack_bootimg" "simg2img" "sload_f2fs" "unpack_bootimg" "zipalign")
 CHECK_TOOLS "${ANDROID_TOOLS_EXEC[@]}" && ANDROID_TOOLS=false
-APKTOOL_EXEC=(
-    "apktool" "apktool.jar"
-)
+APKTOOL_EXEC=("apktool" "apktool.jar")
 CHECK_TOOLS "${APKTOOL_EXEC[@]}" && APKTOOL=false
-EROFS_UTILS_EXEC=(
-    "dump.erofs" "extract.erofs" "fsck.erofs" "fuse.erofs" "mkfs.erofs"
-)
+EROFS_UTILS_EXEC=("dump.erofs" "extract.erofs" "fsck.erofs" "fuse.erofs" "mkfs.erofs")
 CHECK_TOOLS "${EROFS_UTILS_EXEC[@]}" && EROFS_UTILS=false
-IMG2SDAT_EXEC=(
-    "blockimgdiff.py" "common.py" "images.py" "img2sdat" "rangelib.py" "sparse_img.py"
-)
+IMG2SDAT_EXEC=("blockimgdiff.py" "common.py" "images.py" "img2sdat" "rangelib.py" "sparse_img.py")
 CHECK_TOOLS "${IMG2SDAT_EXEC[@]}" && IMG2SDAT=false
-SAMLOADER_EXEC=(
-    "../venv/bin/samloader"
-)
+SAMLOADER_EXEC=("../venv/bin/samloader")
 CHECK_TOOLS "${SAMLOADER_EXEC[@]}" && SAMLOADER=false
-SIGNAPK_EXEC=(
-    "signapk" "signapk.jar"
-)
+SIGNAPK_EXEC=("signapk" "signapk.jar")
 CHECK_TOOLS "${SIGNAPK_EXEC[@]}" && SIGNAPK=false
 
 if [[ "$1" == "--check-tools" ]]; then
-    if ! $ANDROID_TOOLS && \
-            ! $APKTOOL && \
-            ! $EROFS_UTILS && \
-            ! $IMG2SDAT && \
-            ! $SAMLOADER && \
-            ! $SIGNAPK; then
+    if ! $ANDROID_TOOLS && ! $APKTOOL && ! $EROFS_UTILS && ! $IMG2SDAT && ! $SAMLOADER && ! $SIGNAPK; then
         exit 0
     else
         exit 1
     fi
 elif [ "$1" ]; then
     echo "Usage: $(basename "$0" | sed 's/build_dependencies.sh/build_dependencies/')" >&2
-    echo "This script does not accept any arguments." >&2
     exit 1
 fi
 
 if $ANDROID_TOOLS; then
-    ANDROID_TOOLS_CMDS=(
-        "git submodule foreach --recursive \"git am --abort || true\""
-        "cmake -B \"build\" $(GET_CMAKE_FLAGS) -DANDROID_TOOLS_USE_BUNDLED_FMT=ON -DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON"
-        "make -C \"build\" -j\"$(nproc)\""
-        "find \"build/vendor\" -maxdepth 1 -type f -exec test -x {} \; -exec cp -a {} \"$TOOLS_DIR/bin\" \;"
-        "cp -a \"vendor/avb/avbtool.py\" \"$TOOLS_DIR/bin/avbtool\""
-        "cp -a \"vendor/mkbootimg/mkbootimg.py\" \"$TOOLS_DIR/bin/mkbootimg\""
-        "cp -a \"vendor/mkbootimg/repack_bootimg.py\" \"$TOOLS_DIR/bin/repack_bootimg\""
-        "cp -a \"vendor/mkbootimg/unpack_bootimg.py\" \"$TOOLS_DIR/bin/unpack_bootimg\""
-        "cp -a \"vendor/libufdt/utils/src/mkdtboimg.py\" \"$TOOLS_DIR/bin/mkdtboimg\""
-        "mkdir -p \"$TOOLS_DIR/bin/gki\""
-        "cp -a \"vendor/mkbootimg/gki/generate_gki_certificate.py\" \"$TOOLS_DIR/bin/gki/generate_gki_certificate.py\""
-        "ln -sf \"$TOOLS_DIR/bin/mke2fs.android\" \"$TOOLS_DIR/bin/mke2fs\""
-        "cp -a \"../ext4_utils/mkuserimg_mke2fs.py\" \"$TOOLS_DIR/bin/mkuserimg_mke2fs.py\""
-        "ln -sf \"$TOOLS_DIR/bin/mkuserimg_mke2fs.py\" \"$TOOLS_DIR/bin/mkuserimg_mke2fs\""
-        "cp -a \"../ext4_utils/mke2fs.conf\" \"$TOOLS_DIR/bin/mke2fs.conf\""
-        "cp -a \"../f2fs_utils/mkf2fsuserimg.sh\" \"$TOOLS_DIR/bin/mkf2fsuserimg\""
-    )
-
+    ANDROID_TOOLS_CMDS=("git submodule foreach --recursive \"git am --abort || true\"" "cmake -B \"build\" $(GET_CMAKE_FLAGS) -DANDROID_TOOLS_USE_BUNDLED_FMT=ON -DANDROID_TOOLS_USE_BUNDLED_LIBUSB=ON" "make -C \"build\" -j\"$(nproc)\"" "find \"build/vendor\" -maxdepth 1 -type f -exec test -x {} \; -exec cp -a {} \"$TOOLS_DIR/bin\" \;" "cp -a \"vendor/avb/avbtool.py\" \"$TOOLS_DIR/bin/avbtool\"" "cp -a \"vendor/mkbootimg/mkbootimg.py\" \"$TOOLS_DIR/bin/mkbootimg\"" "cp -a \"vendor/mkbootimg/repack_bootimg.py\" \"$TOOLS_DIR/bin/repack_bootimg\"" "cp -a \"vendor/mkbootimg/unpack_bootimg.py\" \"$TOOLS_DIR/bin/unpack_bootimg\"" "cp -a \"vendor/libufdt/utils/src/mkdtboimg.py\" \"$TOOLS_DIR/bin/mkdtboimg\"" "mkdir -p \"$TOOLS_DIR/bin/gki\"" "cp -a \"vendor/mkbootimg/gki/generate_gki_certificate.py\" \"$TOOLS_DIR/bin/gki/generate_gki_certificate.py\"" "ln -sf \"$TOOLS_DIR/bin/mke2fs.android\" \"$TOOLS_DIR/bin/mke2fs\"" "cp -a \"../ext4_utils/mkuserimg_mke2fs.py\" \"$TOOLS_DIR/bin/mkuserimg_mke2fs.py\"" "ln -sf \"$TOOLS_DIR/bin/mkuserimg_mke2fs.py\" \"$TOOLS_DIR/bin/mkuserimg_mke2fs\"" "cp -a \"../ext4_utils/mke2fs.conf\" \"$TOOLS_DIR/bin/mke2fs.conf\"" "cp -a \"../f2fs_utils/mkf2fsuserimg.sh\" \"$TOOLS_DIR/bin/mkf2fsuserimg\"")
     BUILD "android-tools" "$SRC_DIR/external/android-tools" "${ANDROID_TOOLS_CMDS[@]}"
 fi
 if $APKTOOL; then
-    APKTOOL_CMDS=(
-        "git reset --hard"
-        "git apply \"$SRC_DIR/external/patches/apktool/0001-feat-support-aapt-optimization.patch\""
-        "./gradlew build shadowJar"
-        "cp -a \"scripts/linux/apktool\" \"$TOOLS_DIR/bin\""
-        "cp -a \"brut.apktool/apktool-cli/build/libs/apktool-cli.jar\" \"$TOOLS_DIR/bin/apktool.jar\""
-    )
-
-    BUILD "apktool" "$SRC_DIR/external/apktool" "${APKTOOL_CMDS[@]}"
-fi
-if $EROFS_UTILS; then
-    EROFS_UTILS_CMDS=(
-        "cmake -S \"build/cmake\" -B \"out\" $(GET_CMAKE_FLAGS) -DRUN_ON_WSL=\"$(IS_WSL)\" -DENABLE_FULL_LTO=\"ON\" -DMAX_BLOCK_SIZE=\"4096\""
-        "make -C \"out\" -j\"$(nproc)\""
-        "find \"out/erofs-tools\" -maxdepth 1 -type f -exec test -x {} \; -exec cp -a {} \"$TOOLS_DIR/bin\" \;"
-    )
-
-    BUILD "erofs-utils" "$SRC_DIR/external/erofs-utils" "${EROFS_UTILS_CMDS[@]}"
-fi
-if $IMG2SDAT; then
-    IMG2SDAT_CMDS=(
-        "test -f \"img2sdat\""
-        "find \".\" -maxdepth 1 -type f -exec test -x {} \; -exec cp -a {} \"$TOOLS_DIR/bin\" \;"
-    )
-
-    BUILD "img2sdat" "$SRC_DIR/external/img2sdat" "${IMG2SDAT_CMDS[@]}"
-fi
-if $SAMLOADER; then
-    SAMLOADER_CMDS=(
-        "git reset --hard"
-        "git apply \"$SRC_DIR/external/patches/samloader/0001-Update-decryption-keys.patch\""
-        "git apply \"$SRC_DIR/external/patches/samloader/0002-Fix-client-request-params.patch\""
-        "git apply \"$SRC_DIR/external/patches/samloader/0003-Add-timeout-to-version.xml-request.patch\""
-        "python3 -m venv \"$TOOLS_DIR/venv\""
-        "source \"$TOOLS_DIR/venv/bin/activate\"; pip3 install ."
-    )
-
-    BUILD "samloader" "$SRC_DIR/external/samloader" "${SAMLOADER_CMDS[@]}"
-fi
-if $SIGNAPK; then
-    SIGNAPK_CMDS=(
-        "./gradlew build"
-        "cp -a \"scripts/linux/signapk\" \"$TOOLS_DIR/bin\""
-        "cp -a \"signapk/build/libs/signapk-all.jar\" \"$TOOLS_DIR/bin/signapk.jar\""
-    )
-
-    BUILD "signapk" "$SRC_DIR/external/signapk" "${SIGNAPK_CMDS[@]}"
-fi
-
-exit 0
+    APKTOOL_CMDS=("git reset --hard" "git apply \"$SRC_DIR/external/patches/apktool/0
